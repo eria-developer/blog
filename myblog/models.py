28b8e8338bc54bacabd2be8942dcd8b1195099f8
Talemwa Eria Jackson <120446528+Talemwa-Eria-Jackson@users.noreply.github.com>
2024-04-29 13:53:47 +0300

    updated the blog model

--- myblog/models.py
+++ myblog/models.py
@@ -11,6 +11,8 @@ class Blog(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=120)
     description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.title} by {self.author.username}"
